68f0cac16388295b7aaa0fe2dccc2c3e0faaaeaf
Miguel Tinembart <miguel.tinembart@gmail.com>
2025-08-05 21:57:09 +0200

    Initial commit

---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }: 
+{
+
+  home.packages = with pkgs; [ 
+    atool 
+    httpie 
+    neovim 
+    zsh
+    git
+  ];
+
+  programs.bash.enable = false;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  programs.git.config = {
+    user.name = "Miguel Tinembart";
+    user.email = "miguel.tinembart@gmail.com";
+  };
+  
+  # The state version is required and should stay at the version you
+  # originally installed.
+  home.stateVersion = "25.05";
+  
+}
+
